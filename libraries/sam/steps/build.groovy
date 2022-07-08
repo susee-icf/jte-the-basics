b5edc08ee1b1c8bbae516f816f1b59c87800b809
SNatarajan <suseendran.natarajan@icf.com>
2022-07-08 17:24:57 -0400

    Update build.groovy

--- libraries/sam/steps/build.groovy
+++ libraries/sam/steps/build.groovy
@@ -1,5 +1,8 @@
 void call(){
     stage("SAM: Build"){
-        println "build from the SAM library"
+        steps {
+        sh 'python3 -m venv venv && venv/bin/pip install aws-sam-cli'
+        stash includes: '**/venv/**/*', name: 'venv'
+      }
     }
 }
