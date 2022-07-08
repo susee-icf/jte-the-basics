a461d0fb2e7aacf84b209cc57a4cffce35ea0d86
SNatarajan <suseendran.natarajan@icf.com>
2022-07-08 18:38:23 -0400

    Update compile.groovy

--- libraries/sam/steps/compile.groovy
+++ libraries/sam/steps/compile.groovy
@@ -1,6 +1,7 @@
 void call(){
      stage('Build-sam') {
            node {
+             println "hey from the complie step"
              unstash 'venv'
              sh 'venv/bin/sam build'
              stash includes: '**/.aws-sam/**/*', name: 'aws-sam'
