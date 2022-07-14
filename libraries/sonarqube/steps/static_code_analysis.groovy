685ae6ee7ff8d8c93af363a202cdee6ff45460bb
SNatarajan <suseendran.natarajan@icf.com>
2022-07-13 20:06:11 -0400

    Update static_code_analysis.groovy

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,5 +1,5 @@
 void call(){
     stage("SonarQube: Static Code Analysis"){
-        println "static code analysis from the sonarqube library"
+        println "static code analysis from the sonarqube library - version 1.1"
     }
 }
